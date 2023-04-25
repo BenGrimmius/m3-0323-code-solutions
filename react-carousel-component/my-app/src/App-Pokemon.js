ca44601dd7091386ebfc9f4360c9ef4b0563e575
Ben Grimmius <bgrimmdev@gmail.com>
2023-04-25 03:48:26 +0000

    Complete react-carousel-component exercise

---
+++ react-carousel-component/my-app/src/App-Pokemon.js
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronRight } from 'react-icons/fa';
+import { FaRegCircle } from 'react-icons/fa';
+import { FaCircle } from 'react-icons/fa'
+
+
+export default function Container({ items }) {
+  let [current, setCurrent] = useState(0)
+
+  function HandleChevLeft() {
+    setCurrent((current - 1 + items.length) % items.length)
+  }
+
+  function HandleChevRight() {
+    setCurrent((current + 1) % items.length)
+
+  }
+
+  function HandleIndicators(i) {
+    setCurrent(i)
+  }
+
+  let currentPokemonPic = items[current].src
+
+  return (
+    <div className='container'>
+      <div className='row pokemon-font'>
+        <h1 style={{ userSelect: 'none' }}>{items[current].name}</h1>
+      </div>
+      <div className='row'>
+        <ChevLeft OnSelect={() => HandleChevLeft() }/>
+        <PokemonPicture
+          src={ currentPokemonPic }
+          alt={ items[current].name }/>
+        <ChevRight OnSelect={() => HandleChevRight()}/>
+      </div>
+      <div className='row'>
+        <Indicators
+          count={items.length}
+          OnSelect={HandleIndicators}
+          current={current}
+        />
+      </div>
+    </div>
+  )
+}
+
+
+
+function PokemonPicture({ src, alt }) {
+  return (
+    <div style={{userSelect: 'none'}}>
+      <img src={src} alt={alt} />
+    </div>
+  )
+}
+
+
+
+function ChevLeft({ OnSelect }) {
+  return (
+    <div>
+    <FaChevronLeft className='chev' onClick={OnSelect}/>
+    </div>
+  )
+}
+
+function ChevRight({ OnSelect }) {
+  return (
+    <div>
+      <FaChevronRight className='chev' onClick={OnSelect} />
+    </div>
+  )
+}
+
+function Indicators({ count, OnSelect, current }) {
+  const buttons = [];
+  for (let i = 0; i < count; i++) {
+    buttons.push(
+      current === i ?
+      <FaCircle className='indicator'
+      onClick={() => OnSelect(i)}
+      current={current}
+      key={i}/> :
+        <FaRegCircle className='indicator'
+          onClick={() => OnSelect(i)}
+          current={current}
+          key={i} />
+    )
+  }
+  return (
+    <div style={{display: 'flex'}}>
+      {buttons}
+    </div>
+  )
+}
---
+++ react-carousel-component/my-app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Container from './App-Pokemon';
+import bulbasaurPhoto from './images/001.png';
+import charmanderPhoto from './images/004.png';
+import squirtlePhoto from './images/007.png';
+import pikachuPhoto from './images/025.png';
+import jigglypuffPhoto from './images/039.png';
+
+const pokemonList = [
+  { number: '001', name: 'Bulbasaur', src: bulbasaurPhoto, logo: 'https://image.pngaaa.com/548/362548-small.png'},
+  {number: '004', name: 'Charmander', src: charmanderPhoto},
+  {number: '007', name: 'Squirtle', src: squirtlePhoto},
+  {number: '025', name: 'Pikachu', src: pikachuPhoto},
+  {number: '039', name: 'Jigglypuff', src: jigglypuffPhoto}
+]
+
+function App() {
+
+  return (
+    <>
+      <Container items={pokemonList}/>
+    </>
+  );
+}
+
+export default App;
